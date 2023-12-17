7d12f0f762b6815189a71c789e94674d41ba589f
haidarzaki <122019522+haidarzaki@users.noreply.github.com>
2023-12-17 16:17:26 +0700

    go

---
+++ belajar-spring/src/main/java/salathree/belajar/spring/core/models/Pelanggan.java
@@ -0,0 +1,16 @@
+package salathree.belajar.spring.core.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
